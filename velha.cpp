f1388268405609873984afaebcdd30d30452a80c
Arthur Neves <arthurneves0512@gmail.com>
2025-09-23 20:28:14 -0300

    Testa velha

--- velha.cpp
+++ velha.cpp
@@ -16,20 +16,16 @@
  */
 using namespace std;
 int VerificaVelha(int velha[3][3]) {
-  for (int i = 0; i < 3; i++) {
-    bool horizontal_match =
-        velha[i][0] == velha[i][1] && velha[i][0] == velha[i][2];
-    bool vertical_match =
-        velha[0][i] == velha[1][i] && velha[0][i] == velha[2][i];
-    if (horizontal_match || vertical_match) {
-      return velha[i][0];
-    }
+  if (isImpossible(velha)) {
+    return -2;
   }
-  if ((velha[0][0] == velha[1][1] && velha[1][1] == velha[2][2]) ||
-      (velha[2][2] == velha[0][2] && velha[2][2] == velha[1][1])) {
-    return velha[1][1];
+  if (!isFinishedw(velha)) {
+    return -1;
   }
-  return 0;
+  if (isDraw(velha)) {
+    return 0;
+  }
+  return Winner(velha);
 }
 bool isDraw(int game[3][3]) { return VerificaVelha(game) == 0 ? true : false; }
 bool isImpossible(int game[3][3]) {
