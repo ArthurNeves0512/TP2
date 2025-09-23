4da1c3238b1851d66ddacd1809ec647ba01108ea
Arthur Neves <arthurneves0512@gmail.com>
2025-09-23 15:08:55 -0300

    Testa empate

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -10,3 +10,7 @@ TEST_CASE("Testa velha", "[single-file]") {
   REQUIRE(VerificaVelha(teste_X) == VENCEDOR_X);
   REQUIRE(VerificaVelha(teste_0) == VENCEDOR_O);
 }
+TEST_CASE("Testa empate", "[valido]") {
+  int teste_empate[3][3] = {{1, 2, 1}, {2, 1, 2}, {1, 1, 2}};
+  REQUIRE(isDraw(teste_empate) == 0);
+}
--- velha.hpp
+++ velha.hpp
@@ -1,1 +1,2 @@
 int VerificaVelha(int velha[3][3]);
+bool isDraw(int game[3][3]);
