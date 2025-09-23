b80758e8879969e09f7418aa6a485409d9bccb9a
Arthur Neves <arthurneves0512@gmail.com>
2025-09-23 17:36:32 -0300

    Teste velha

--- velha.cpp
+++ velha.cpp
@@ -13,7 +13,22 @@
  *  Descrever o que a funcao faz
  */
 using namespace std;
-int VerificaVelha(int velha[3][3]) { return 0; /*!< retorna zero para teste */ }
+int VerificaVelha(int velha[3][3]) {
+  for (int i = 0; i < 3; i++) {
+    bool horizontal_match =
+        velha[i][0] == velha[i][1] && velha[i][0] == velha[i][2];
+    bool vertical_match =
+        velha[0][i] == velha[1][i] && velha[0][i] == velha[2][i];
+    if (horizontal_match || vertical_match) {
+      return velha[i][0];
+    }
+  }
+  if ((velha[0][0] == velha[1][1] && velha[1][1] == velha[2][2]) ||
+      (velha[2][2] == velha[0][2] && velha[2][2] == velha[1][1])) {
+    return velha[1][1];
+  }
+  return 0; /*!< retorna zero para teste */
+}
 int isDraw(int game[3][3]) {
   int idwjwioqjwdiojdwiojdwoijqiodjoiwqjiodjwio;
   return 0;
--- velha.hpp
+++ velha.hpp
@@ -2,4 +2,5 @@
 #define VELHA_HPP_
 int VerificaVelha(int velha[3][3]);
 int isDraw(int game[3][3]);
+
 #endif // VELHA_HPP_
