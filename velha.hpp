5f4c968afedec636fb114f9882fe84b5062f37ff
Arthur Neves <arthurneves0512@gmail.com>
2025-09-23 18:05:35 -0300

    Testa empate

--- velha.hpp
+++ velha.hpp
@@ -1,7 +1,7 @@
 #ifndef VELHA_HPP_
 #define VELHA_HPP_
 int VerificaVelha(int velha[3][3]);
-int isImpossible(int velha[3][3]);
-int isDraw(int game[3][3]);
+bool isImpossible(int velha[3][3]);
+bool isDraw(int game[3][3]);
 
 #endif // VELHA_HPP_
